30cc7d76d59f06f2df5ad23225ec143841cf4dd6
jimmy-glitch <76778937+AHMED-GAMAL-AG@users.noreply.github.com>
2023-02-28 15:00:37 +0200

    02. إعداد موجهات الواجهة البرمجية - Routes

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Lesson;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -16,4 +17,34 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+// get all lessons
+Route::get('lessons', function () {
+    return Lesson::all();
+});
+
+// get a specific lesson
+Route::get('lessons/{id}', function ($id) {
+    return Lesson::find($id);
+});
+
+// post a lesson
+Route::post('lessons', function (Request $request) {
+    return Lesson::create($request->all());
+});
+
+// edit a post by id
+Route::put('lessons/{id}', function (Request $request, $id) {
+    $lesson = Lesson::findOrFail($id);
+    $lesson->update($request->all());
+
+    return $lesson;
+});
+
+// delete a post by id
+Route::delete('lessons/{id}', function ($id) {
+    $lesson = Lesson::find($id)->delete();
+
+    return 204; // means that the request was successful and the server has fulfilled the request,
 });
